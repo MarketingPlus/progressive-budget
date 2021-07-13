6778554003ba86f9bb8c00f5e162f74df1744800
MarketingPlus <benmarketingplus@gmail.com>
2021-07-13 14:49:17 +1000

    if the responce was good, clone it and store it in the cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -47,4 +47,11 @@ self.addEventListener("fetch", function (evt) {
             caches.open(DATA_CACHE_NAME).then(cache => {
                 return fetch(evt.request)
                     .then(response => {
+                        if (response.status === 200) {
+                            cache.put(evt.request.url, response.clone());
+                        }
+
+                        return response;
+                    })
+                    .catch(err => {
                        
