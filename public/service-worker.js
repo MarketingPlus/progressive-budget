f43922ef6cb0e3587647163b51e5e3c7e6ab707e
MarketingPlus <benmarketingplus@gmail.com>
2021-07-13 14:51:03 +1000

    if the request is not for the API, serve static assests using the offline-first approach

--- public/service-worker.js
+++ public/service-worker.js
@@ -61,3 +61,10 @@ self.addEventListener("fetch", function (evt) {
 
         return;
     }
+
+    evt.respondWith(
+        caches.match(evt.request).then(function (response) {
+            return response || fetch(evt.request);
+        })
+    );
+});
