a1825defaec4305eeb4b5ce706df6446d6696efe
MarketingPlus <benmarketingplus@gmail.com>
2021-07-13 15:01:57 +1000

    added some comments so i know where items are and what each functions do

--- public/service-worker.js
+++ public/service-worker.js
@@ -13,6 +13,8 @@ const FILES_TO_CACHE = [
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
+// TODO: add listener and handler to retrieve static assets from the Cache Storage in the browser 
+// install
 self.addEventListener("install", function (evt) {
     evt.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
@@ -41,12 +43,15 @@ self.addEventListener("activate", function (evt) {
     self.clients.claim();
 });
 
+// fetch
 self.addEventListener("fetch", function (evt) {
+    // cache successful requests to the API
     if (evt.request.url.includes("/api/")) {
         evt.respondWith(
             caches.open(DATA_CACHE_NAME).then(cache => {
                 return fetch(evt.request)
                     .then(response => {
+                        // If the response was good, clone it and store it in the cache.
                         if (response.status === 200) {
                             cache.put(evt.request.url, response.clone());
                         }
@@ -54,6 +59,7 @@ self.addEventListener("fetch", function (evt) {
                         return response;
                     })
                     .catch(err => {
+                        // Network request failed, try to get it from the cache.
                         return cache.match(evt.request);
                     });
             }).catch(err => console.log(err))
@@ -62,6 +68,7 @@ self.addEventListener("fetch", function (evt) {
         return;
     }
 
+    // if the request is not for the API, serve static assets using "offline-first" approach.
     evt.respondWith(
         caches.match(evt.request).then(function (response) {
             return response || fetch(evt.request);
